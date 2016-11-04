c002d243b74654921030116fd892be31471febb6
Yuri <vetrov.y@bdc.mtk.by>
2016-11-04 15:17:53 +0300

    added KillProcessAction class

---
+++ HookSample/HookSample.Core/Actions/KillProcessAction.cs
@@ -0,0 +1,57 @@
+﻿//
+// Yuri Vetroff
+// yuri.vetroff@gmail.com
+//
+
+using System;
+using System.Diagnostics;
+
+namespace HookSample.Core.Actions
+{
+    public class KillProcessAction : ProcessAction
+    {
+        /// <summary>
+        /// Initializes a new instance of the KillProcessAction class with the specified process name.
+        /// </summary>
+        /// <param name="processName">The name of the process to kill.</param>
+        public KillProcessAction(string processName)
+            : base(processName)
+        { }
+
+        /// <summary>
+        /// Executes the action.
+        /// </summary>
+        public override void Execute()
+        {
+            // Calls base method.
+            base.Execute();
+
+            try
+            {
+                // Gets the list with processes by the process name.
+                var processes = Process.GetProcessesByName(processName);
+
+                // Checks if the list is empty and writes to log the corresponding error message.
+                if (processes.Length == 0)
+                    Log.Instance.Write("Process kill error: no processes found for " + processName);
+                // Otherwise, kills the every process in the list.
+                else
+                    Array.ForEach(processes, p => p.Kill());
+            }
+            catch (Exception ex)
+            {
+                // Writes the error in the log.
+                Log.Instance.Write("Process kill error: " + processName + " - " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Returns a string that represents the KillProcessAction.
+        /// </summary>
+        /// <returns>A string that represents the KillProcessAction.</returns>
+        public override string ToString()
+        {
+            return "kill" + base.ToString();
+        }
+    }
+}
