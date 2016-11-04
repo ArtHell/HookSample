8766dd38f7237881868f0fc5378c3404e72d965d
Yuri <vetrov.y@bdc.mtk.by>
2016-11-04 14:37:03 +0300

    added Log singleton class

---
+++ HookSample/HookSample.Core/Log.cs
@@ -0,0 +1,81 @@
+﻿//
+// Yuri Vetroff
+// yuri.vetroff@gmail.com
+//
+
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace HookSample.Core
+{
+    internal class Log
+    {
+        #region Singleton members
+
+        /// <summary>
+        /// The instance of the Log.
+        /// </summary>
+        private static Log instance = null;
+        /// <summary>
+        /// Initializes a new instance of the Log class.
+        /// </summary>
+        private Log() { }
+        /// <summary>
+        /// Gets the instance of the Log.
+        /// </summary>
+        public static Log Instance
+        {
+            get
+            {
+                // Checks if the instance is null.
+                if (instance == null)
+                    // Creates the instance if yes.
+                    instance = new Log();
+
+                // Returns the instance.
+                return instance;
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Gets or sets the path to the log file.
+        /// </summary>
+        public string Filename { get; set; } = "log.txt";
+
+        /// <summary>
+        /// Writes the the specified text to the log.
+        /// </summary>
+        /// <param name="text">The text to write.</param>
+        public void Write(string text)
+        {
+            // Gets the current time in hh.mm.ss format.
+            var time = DateTime.Now.ToLongTimeString();
+            // Adds the time to the text string (before the text).
+            text = time + " - " + text;
+            // Opens the log file and writes the text.
+            try
+            {
+                // Creates a stream writer and writes the text.
+                using (var writer = new StreamWriter(Filename, true))
+                    writer.WriteLine(text);
+            }
+            catch { }
+        }
+
+        /// <summary>
+        /// Writes the specified number to the log.
+        /// </summary>
+        /// <param name="number">The number to write.</param>
+        public void Write(int number)
+        {
+            // Converts the number to a string value (firstly getting the key assigned to it).
+            var text = ((Keys)number).ToString();
+
+            // Writes it as a usual text.
+            Write(text);
+        }
+    }
+}
