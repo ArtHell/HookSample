4076072ca9969d4bd051e9b513ea4ddf77c1eb51
Yuri <vetrov.y@bdc.mtk.by>
2016-11-04 14:42:46 +0300

    added KeyPressedEventArgs class for key pressed events

--- HookSample/HookSample.Core/ExtendedKeyboardHook.cs
+++ HookSample/HookSample.Core/ExtendedKeyboardHook.cs
@@ -3,8 +3,20 @@
 // yuri.vetroff@gmail.com
 //
 
+using System;
+
 namespace HookSample.Core
 {
+    public class KeyPressedEventArgs : EventArgs
+    {
+        public KeyPressedEventArgs(int key)
+        {
+            Key = key;
+        }
+
+        public int Key { get; private set; }
+    }
+
     public class ExtendedKeyboardHook : KeyboardHook
     {
     }
