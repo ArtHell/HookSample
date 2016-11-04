1e79a40f21213158d6686c76473d00ace368e520
Yuri <vetrov.y@bdc.mtk.by>
2016-11-04 15:39:03 +0300

    added ActionSequence

---
+++ HookSample/HookSample.Core/Actions/ActionSequence.cs
@@ -0,0 +1,31 @@
+﻿//
+// Yuri Vetroff
+// yuri.vetroff@gmail.com
+//
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace HookSample.Core.Actions
+{
+    /// <summary>
+    /// Provides a collection of actions.
+    /// </summary>
+    public class ActionSequence : List<IHookAction>
+    {
+        /// <summary>
+        /// Returns a string that represents the ActionSequence.
+        /// </summary>
+        /// <returns>A string that represents the ActionSequence.</returns>
+        public override string ToString()
+        {
+            // Creates a new string builder and appends every action string to it.
+            var builder = new StringBuilder();
+            foreach (var action in this)
+                builder.AppendLine("   " + action.ToString());
+
+            // Returns the builder value.
+            return builder.ToString();
+        }
+    }
+}
