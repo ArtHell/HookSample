c9a278724409cf1a292d8ca549843fe8f9826063
Yuri <vetrov.y@bdc.mtk.by>
2016-11-08 12:08:00 +0300

    applied in main method

--- HookSample/HookSample.UI/Program.cs
+++ HookSample/HookSample.UI/Program.cs
@@ -3,13 +3,27 @@
 // yuri.vetroff@gmail.com
 //
 
+using HookSample.Core;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace HookSample.UI
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using (var hook = new ExtendedKeyboardHook())
+            {
+                var task = new Task(() =>
+                {
+                    var ui = new KeyboardHookConsoleUI(hook);
+                    ui.MainMenu();
+                });
+                task.Start();
 
+                Application.Run();
+            }
         }
     }
 }
