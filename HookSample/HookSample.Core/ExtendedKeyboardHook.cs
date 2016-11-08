34b12ada463893cec2dc65058fc67ffb9c46e103
Yuri <vetrov.y@bdc.mtk.by>
2016-11-08 11:59:36 +0300

    removed KeyPressed event and its support

--- HookSample/HookSample.Core/ExtendedKeyboardHook.cs
+++ HookSample/HookSample.Core/ExtendedKeyboardHook.cs
@@ -10,41 +10,10 @@ using System.Collections.Generic;
 namespace HookSample.Core
 {
     /// <summary>
-    /// Contains event data for key pressed events.
-    /// </summary>
-    public class KeyPressedEventArgs : EventArgs
-    {
-        /// <summary>
-        /// Initializes a new instance of the KeyPressedEventArgs class.
-        /// </summary>
-        /// <param name="key">A key that has been pressed.</param>
-        public KeyPressedEventArgs(int key)
-        {
-            // Sets the key.
-            Key = key;
-        }
-
-        /// <summary>
-        /// Gets the key that has been pressed.
-        /// </summary>
-        public int Key { get; private set; }
-    }
-
-    /// <summary>
-    /// Represents an extended keyboard hook that provides events of a key pressing
-    /// and actions sequences that are binded to keys.
+    /// Represents an extended keyboard hook that provides actions sequences binded to keys.
     /// </summary>
     public class ExtendedKeyboardHook : KeyboardHook
     {
-        /// <summary>
-        /// The delegate for the KeyPressed event.
-        /// </summary>
-        public delegate void KeyPressedHandler(object sender, KeyPressedEventArgs e);
-        /// <summary>
-        /// Occurs when a key pressed during the working hook.
-        /// </summary>
-        public event KeyPressedHandler KeyPressed;
-
         /// <summary>
         /// Gets the dictionary with keys and corresponding action sequences.
         /// </summary>
@@ -58,9 +27,6 @@ namespace HookSample.Core
         {
             // Calls base method.
             base.CallbackCore(vkCode);
-
-            // Invokes handler of the event.
-            KeyPressed?.Invoke(this, new KeyPressedEventArgs(vkCode));
 
             // Manages actions.
             ManageActions(vkCode);
